d79b7307524b99784fcc557bced97b0c97207891
mankaki <32032046+mankaki@users.noreply.github.com>
2026-01-11 21:17:23 +0800

    Update and rename auto-close-danmu.js to auto-close-danmu.user.js
    更新描述

--- auto-close-danmu.user.js
+++ auto-close-danmu.user.js
@@ -7,6 +7,19 @@
 // @match        *://www.mgtv.com/*
 // @grant        none
 // @license      GPL-3.0
+
+// ==UserScript==
+// @name         芒果TV网页版自动关闭弹幕
+// @namespace    http://tampermonkey.net/
+// @version      1.17.4
+// @description  自动关闭芒果TV视频弹幕，支持切换集数后自动关闭弹幕，用户可选择启用或禁用功能，支持快捷键 D 手动开启/关闭弹幕
+// @author       mankaki
+// @match        *://www.mgtv.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=mgtv.com
+// @grant        none
+// @license      GPL-3.0
+// @downloadURL https://raw.githubusercontent.com/mankaki/auto-close-danmu/main/auto-close-danmu.user.js
+// @updateURL https://raw.githubusercontent.com/mankaki/auto-close-danmu/main/auto-close-danmu.user.js
 // ==/UserScript==
  
 (function () {
